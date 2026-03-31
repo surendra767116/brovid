5d0bc9d880aeb5780c1dbf428a85c416617f6247
BYSANI SURENDRA REDDY <99240040739@klu.ac.in>
2026-03-31 19:49:50 +0530

    Allow all hosts for deployment

--- shirtverse/settings.py
+++ shirtverse/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY", default="dev-only-secret-key-change-me")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
