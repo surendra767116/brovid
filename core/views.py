07ef2a415fe2e14ceb400b53450c0ad816b315e0
BYSANI SURENDRA REDDY <99240040739@klu.ac.in>
2026-03-31 20:01:02 +0530

    Show latest active shirts on home page

--- core/views.py
+++ core/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
-from products.models import Category
+from products.models import Category, Product
 
 
 def home(request):
 	categories = Category.objects.all()
+	recent_products = (
+		Product.objects.filter(is_active=True)
+		.select_related("category")
+		.order_by("-created_at")[:8]
+	)
 	context = {
 		"categories": categories,
+		"recent_products": recent_products,
 	}
 	return render(request, "home.html", context)
 
